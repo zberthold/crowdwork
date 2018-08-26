003e586f1595a50c94793fa4b7a8b2aee6de14e5
Zoe Berthold <zoeberthold@dhcp-1262.guestnet.imsa.edu>
2018-08-25 22:54:19 -0500

    finished setting up segues

--- crowdwork/ViewController.swift
+++ crowdwork/ViewController.swift
@@ -19,6 +19,18 @@ class ViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "hey"
+        cell.detailTextLabel?.text = "baby"
+        return cell
+
+
+    }
 
 
 }
